fa89eca276cfd184c0ea8deaa560198f3e3f6000
이경진 <rudwls1378@gmail.com>
2021-07-21 21:30:19 +0900

    node.js 서버와 연동하는 클래스

--- src/main/java/com/binfree/web/main/controller/HomeController.java
+++ src/main/java/com/binfree/web/main/controller/HomeController.java
@@ -7,6 +7,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Locale;
 
+import org.json.simple.JSONObject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,6 +17,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -42,30 +44,29 @@ import lombok.extern.log4j.Log4j;
 @Controller
 @AllArgsConstructor
 public class HomeController {
-	
+
 	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
-	
+
 	private MainService mainService;
-	
+
 	private ReviewService service;
-	
+
 	private BuddyService buddyService;
-	
+
 	private UsersService userService;
-	
-	
+
 	/**
 	 * Simply selects the home view to render by returning its name.
 	 */
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String home(Model model, Criteria cri) {
-		
+
 		// 요한
 		int total = service.getTotal();
 		int star = service.getStarTotal();
 		int starSum = star / total;
-		
-		//버디수
+
+		// 버디수
 		int buddyTotal = buddyService.getTotalCountForHome();
 
 		int userTotal = userService.userCount();
@@ -77,65 +78,56 @@ public class HomeController {
 
 		model.addAttribute("pageMaker", pageMaker);
 		model.addAttribute("starSum", starSum);
-		model.addAttribute("buddyTotal", buddyTotal );
+		model.addAttribute("buddyTotal", buddyTotal);
 		model.addAttribute("userTotal", userTotal);
-		
-		
+
 		// 승연
 		List<ReviewVO> list = mainService.getReviewList();
-		
+
 		model.addAttribute("list", list);
-		
+
 		System.out.println(list);
-		
-		
+
 		return "home";
-	}	
+	}
 
 	@RequestMapping(value = "/service", method = RequestMethod.GET)
 	public void service() {
-		
-	}	
-	
-	@RequestMapping(value="/intro", method = RequestMethod.GET)
-	public void intro() {
-		
+
 	}
 
-		@PostMapping("getUserInfo")
-		@ResponseBody
-		public Model header(Model model) throws JsonProcessingException {
-			
-			Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-			
-			if (authentication != null) {
-				CustomUserDetails loginUserVO = (CustomUserDetails) authentication.getPrincipal();
-				CustomUserDetails loginUserInfo = new CustomUserDetails();
-				
-				loginUserInfo = userService.getLoginUserInfo(loginUserVO.getUsername());
-				
-				CustomUserDetails user = new CustomUserDetails();
-				user = userService.getLoginUserInfo(loginUserVO.getUsername());
-				
-				model.addAttribute("user", user);
-				
-				return model;
-				
-//				HashMap<String, Object> hashmap = new HashMap<String, Object>();
-//				hashmap.put("user", user);
-//				ObjectMapper mapper = new ObjectMapper();
-//				String json = mapper.writerWithDefaultPrettyPrinter().writeValueAsString(hashmap);
-//				
-//				return json;
-				
-			} else {
-				
-				return null;
-				
-			}
-		
+	@RequestMapping(value = "/intro", method = RequestMethod.GET)
+	public void intro() {
+
+	}
+
+	@PostMapping("getUserInfo")
+	@ResponseBody
+	public Model header(Model model) throws JsonProcessingException {
+
+		Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+		if (authentication != null) {
+			CustomUserDetails loginUserVO = (CustomUserDetails) authentication.getPrincipal();
+			CustomUserDetails loginUserInfo = new CustomUserDetails();
+
+			loginUserInfo = userService.getLoginUserInfo(loginUserVO.getUsername());
+
+			CustomUserDetails user = new CustomUserDetails();
+			user = userService.getLoginUserInfo(loginUserVO.getUsername());
+
+			model.addAttribute("user", user);
+
+			return model;
+
+		} else {
+
+			return null;
+
+		}
+
 	}
 	
 	
-	
+
 }
